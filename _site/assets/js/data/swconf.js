9a4c32fecbc7a73d4fc8cd36bcea8b1fffb5bdfc
root <root@Test-Jekyll-Applitools.es>
2026-05-08 08:51:15 +0000

    start

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1778229464',resources: [
+    cacheName: 'chirpy-1778230266',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
