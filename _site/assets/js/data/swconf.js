f206e777a8a7b3c2b58b27f5759c9ded823062a5
root <root@Test-Jekyll-Applitools.es>
2026-05-11 08:51:51 +0000

    cantidad de workers por defecto

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1778488801',resources: [
+    cacheName: 'chirpy-1778489470',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
